c24fdbeacb1b20bec1a00a6c65e876337f63662b
sakshimudgul <sakshimudgul8652@gmail.com>
2026-08-07 18:55:10 +0530

    packages instllation

--- src/routes/receptionist/idRoutes.js
+++ src/routes/receptionist/idRoutes.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const idController = require('../../controllers/receptionist/idController');
+const auth = require('../../middleware/auth');
+const { isReceptionist } = require('../../middleware/roleCheck');
 
-// ID card management
-router.get('/', idController.getIdCards);
-router.post('/', idController.createIdCard);
-router.get('/:id', idController.getIdCard);
-router.put('/:id', idController.updateIdCard);
-router.delete('/:id', idController.deleteIdCard);
+// All routes require authentication and Receptionist role
+router.use(auth);
+router.use(isReceptionist);
 
-// ID card generation
-router.post('/generate', idController.generateIdCards);
-router.get('/print/:id', idController.printIdCard);
-
-module.exports = router;const express = require('express');
-const router = express.Router();
-const idController = require('../../controllers/receptionist/idController');
-
-// ID card management
+// ID card management routes
 router.get('/', idController.getIdCards);
 router.post('/', idController.createIdCard);
 router.get('/:id', idController.getIdCard);
